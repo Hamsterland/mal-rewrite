77124aec2f2d8b447870074ea754b5c2fd08a19b
= <matthewtrip2@gmail.com>
2020-12-05 12:04:24 +0000

    Dynamic routes

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,16 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Home from './Home';
 import { Route, Switch } from 'react-router-dom';
 import Alex from './Alex';
+import User from '../models/User';
+
+const userIds: string[] = [
+  // Squall
+  '226618912320520192',
+
+  // Tofu
+  '249855890381996032',
+
+  // Alex
+  '343201768668266496',
+
+  // Jeremy
+  '754681236236140666',
+
+  // Maddo
+  '711211838305599538',
+
+  // Arden
+  '375573674306306050',
+
+  // Neo
+  '718874307316678698',
+
+  // Mytho
+  '480092510505402380',
+
+  // Yuun
+  '630427600220717067',
+
+  // Uchuu
+  '330746772378877954'
+];
+
+export const Users: User[] = [];
 
 const App = () => {
+
+    const [isBusy, setBusy] = useState(true);
+
+    useEffect(() => {
+        setBusy(true);
+        const fetchData = async() => {
+            for (const id of userIds) {
+                const response = await fetch('https://api.codetabs.com/v1/proxy/?quest=http://hamsterland.herokuapp.com/api/users?id=' + id);
+                if (!response.ok) {
+                    continue;
+                }
+                const user: User = await response.json(); 
+                Users.push(user);
+            }
+
+            setBusy(false);
+        }
+
+        fetchData();
+    }, [])
+    
   return (
-    <main>
-      <Switch>
-        <Route path="/" component={Home} exact/>
-        <Route path="/alex" component={Alex}/>
-      </Switch>
-    </main>
+      <main>
+          <Switch>
+              <Route path="/" component={Home} exact/>
+              {Users.map(user => {
+                  return <Route path="/alex" component={Alex}/>
+              })}
+          </Switch>
+      </main>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import UserButton from '../components/UserButton';
-import User from '../models/User';
 import { Icon } from '@iconify/react-with-api';
 
 import background from './../images/background.png';
 import styles from '../styles/Home.module.scss';
+import { Users } from './App';
+
 
 const discordLink = 'https://discord.gg/UsXvSUh7qu';
 const githubLink = 'https://github.com/Hamsterland/mal-rewrite/';
@@ -12,57 +13,9 @@ const githubLink = 'https://github.com/Hamsterland/mal-rewrite/';
 const discordIcon = 'logos:discord';
 const githubIcon = 'ant-design:github-filled';
 
-const userIds: string[] = [
-    // Squall
-    '226618912320520192',
-
-    // Tofu
-    '249855890381996032',
-
-    // Alex
-    '343201768668266496',
-
-    // Jeremy
-    '754681236236140666',
-
-    // Maddo
-    '711211838305599538',
-
-    // Arden
-    '375573674306306050',
-
-    // Neo
-    '718874307316678698',
-
-    // Mytho
-    '480092510505402380',
-
-    // Yuun
-    '630427600220717067'
-];
 
 const Home = () => {
     
-    const [users, setUsers] = useState<User[]>([]);
-    const [isBusy, setBusy] = useState(true);
-
-    useEffect(() => {
-        setBusy(true);
-        const fetchData = async() => {
-            let users: User[] = [];
-            for (const id of userIds) {
-                const response = await fetch('https://api.codetabs.com/v1/proxy/?quest=http://hamsterland.herokuapp.com/api/users?id=' + id);
-                const user: User = await response.json(); 
-                users.push(user);
-            }
-
-            setUsers(users);
-            setBusy(false);
-        }
-
-        fetchData();
-    }, [])
-
     return (
         <div className="App">
             <div className={styles.title}>
@@ -77,7 +30,7 @@ const Home = () => {
             <div className={styles.navigation}>
                 <div className={styles.bar}>
                     <ul className={styles.elements}>
-                        {users && users.map(user => {
+                        {Users && Users.map(user => {
                           return <li><UserButton name={user.username} avatar={user.avatar}/></li>
                         })}
                     </ul>
